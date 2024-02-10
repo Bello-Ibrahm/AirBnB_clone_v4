0bf3740798632a6b626c850113c79de3f5c1db57
an-nur-info-tech <wasiubello60@gmail.com>
2024-02-10 05:51:11 -0500

    Task 2. Select some Amenities to be comfortable

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,11 @@
 $(document).ready(() => {
-  
+  let amenities = {};
+  $('div.popover ul li input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('div.amenities h4').text(Object.values(amenities).join(', '));
+  });
 });
